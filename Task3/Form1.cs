1bb5950230042b432793ecdda9d7c565868937ce
Artem <artemzhdanov1299@gmail.com>
2023-04-24 00:16:08 +0300

    final

---
+++ Task3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string Value
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.FileName = "";
+            openFileDialog1.InitialDirectory = "C:\\Users\\artem\\OneDrive\\Рабочий стол";
+            openFileDialog1.Multiselect = false;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.MainForm = this;
+            form2.Dialog = Value;
+            form2.Show();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task3/Form2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form2 : Form
+    {
+        public Form1 MainForm { get; set; }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public string Dialog
+        {
+            get { return textBox1.Text; }
+            set { textBox1.Text = value; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm.Value = Dialog;
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
